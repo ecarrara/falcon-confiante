c983fce83f9562c173ae82ec5630803f5ac326f5
Erle Carrara <carrara.erle@gmail.com>
2019-05-21 16:43:30 -0300

    Support new router API in Falcon >= 2

--- falcon_confiante/router.py
+++ falcon_confiante/router.py
@@ -1,11 +1,15 @@
 import importlib
 from collections import defaultdict
+import falcon
 from falcon.routing.compiled import CompiledRouter
+
+
+_falcon_version = [int(d) for d in falcon.__version__.split('.')]
 
 
 class OpenApiRouter(CompiledRouter):
 
-    __slots__ = ("root_package", "resources", "strict_mode")
+    __slots__ = ("root_package", "resources", "strict_mode", "mapping")
 
     def __init__(self, package, openapi_spec: dict, strict_mode: bool = True):
         super().__init__()
@@ -13,10 +17,9 @@ class OpenApiRouter(CompiledRouter):
         self.root_package = package
         self.resources = {}
         self.strict_mode = True
+        self.mapping = defaultdict(lambda: {})  # resource -> method -> fn
 
         for path, http_methods in openapi_spec["paths"].items():
-            mapping = defaultdict(lambda: defaultdict(lambda: {}))
-
             for http_method, defn in http_methods.items():
                 operationId = defn.get("operationId")
                 if operationId is None:
@@ -34,14 +37,22 @@ class OpenApiRouter(CompiledRouter):
                     resource = self.get_resource(module, class_)
                     method = getattr(resource, method_name)
 
-                    mapping[resource][http_method.upper()] = method
+                    self.mapping[resource][http_method.upper()] = method
                 except (AttributeError, ModuleNotFoundError) as exc:
                     raise RuntimeError(
                         f"Error registering endpoint {http_method.upper()} {path}: {exc}"
                     )
 
-            for resource, method_map in mapping.items():
-                self.add_route(path, method_map, resource)
+            for resource, method_map in self.mapping.items():
+                if _falcon_version[0] >= 2:
+                    self.add_route(path, resource)
+                else:
+                    self.add_route(path, self.mapping[resource], resource)
+
+    # override custom mapping strategy
+    def map_http_methods(self, resource, **kwargs):
+        # falcon >= 2 only
+        return self.mapping[resource]
 
     def get_resource(self, module, class_):
         if (module, class_) not in self.resources:
